e9bfc4e01d02e2c1dcd88407696bd56350b58f14
mandowaraharshitaa <91795687+mandowaraharshitaa@users.noreply.github.com>
2024-12-02 20:06:58 +0530

    Update multer.js

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -4,7 +4,7 @@ import path from "path";
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, "uploads");
   },
   filename(req, file, cb) {
     const id = uuid();
