4d3c48f073d0a3f43aca4972f50b8dc65bd26689
mandowaraharshitaa <91795687+mandowaraharshitaa@users.noreply.github.com>
2024-12-07 18:00:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 // using middlewares
 app.use(express.json());
 app.use(cors({
-  origin: 'http://http://talentflexsolution.in'  // Replace with your frontend URL
+  origin: 'https://talentflexsolution.in'  // Replace with your frontend URL
 }));
 app.use("/uploads", express.static("uploads"));
 const port = process.env.PORT;
