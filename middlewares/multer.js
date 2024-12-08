33622119457b8e89971a6e16bb7dfcac43f012ff
mandowaraharshitaa <91795687+mandowaraharshitaa@users.noreply.github.com>
2024-12-08 09:57:00 +0530

    Update multer.js

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,6 +1,5 @@
 import multer from "multer";
 import { v4 as uuid } from "uuid";
-import path from "path";
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
@@ -8,20 +7,14 @@ const storage = multer.diskStorage({
   },
   filename(req, file, cb) {
     const id = uuid();
-    const extName = path.extname(file.originalname); // Safely get the extension
-    const fileName = `${id}${extName}`;
+
+    const extName = file.originalname.split(".").pop();
+
+    const fileName = `${id}.${extName}`;
+
     cb(null, fileName);
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  // Accept both video and image files
-  if (file.mimetype.startsWith("image/") || file.mimetype.startsWith("video/")) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only image and video files are allowed!"), false);
-  }
-};
-
-export const uploadFiles = multer({ storage, fileFilter }).single("file");
+export const uploadFiles = multer({ storage }).single("file");
 
